ead7f804e8e3f183114515d2b56edeadcf3eadb8
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2025-05-21 01:10:42 +0530

    dsv

--- setup.js
+++ setup.js
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 export default function init() {
   try {
     execSync('npm install husky');
-    execSync('husky init');
+    execSync('npx husky init');
     execSync('husky')
     console.log("✅ Husky installed and pre-commit hook added.");
     fs.writeFileSync("./.husky/pre-commit", "npx envwarden check", { mode: 0o755 });
