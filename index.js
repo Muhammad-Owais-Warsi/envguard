45888233564b9d024b34eeb1c1c820486d68282b
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2025-05-20 23:49:28 +0530

    chnages

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 
 import fs from "fs";
+import { execSync } from "child_process";
 
 function init() {
   try {
-    console.log("sdv")
+    execSync('npm install husky');
+    execSync('husky init');
+    execSync('husky')
+    console.log("✅ Husky installed and pre-commit hook added.");
     fs.writeFileSync("./.husky/pre-commit", "node script.js", { mode: 0o755 });
     return;
   } catch(e) {
